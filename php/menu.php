6679d2ba437babea80653977920e729be2b6fbb8
grabinow2 <grabinow2@student.touro.edu>
2020-03-01 14:35:18 -0500

    linked one of the menu items in menu.php to the form

--- php/menu.php
+++ php/menu.php
@@ -1,6 +1,6 @@
 <P>THE MENU</P>
 <ul>
-    <li><a href="...">First Page</a>
+    <li><a href="form.php">Form</a>
     <li><a href="...">Second Page</a>
     <li><a href="...">Third Page</a>
     <li><a href="...">Fourth Page</a>
