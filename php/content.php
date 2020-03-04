6228d949ac290454e14722170936b49926bd72b8
Jacob Burstyn <jacobburstyn@gmail.com>
2020-03-03 23:01:27 -0500

    added login page response page and content page.  As per module 5

---
+++ php/content.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$loggedIn = $_SESSION['LoggedIn'];
+
+if(!$loggedIn){
+    //display error and link to login page
+    echo 'Wrong username or password:   Try again     <a href="login.php">Login Page</a>';
+
+}
+else{
+    echo "Hello " . $_COOKIE['username'];
+}
