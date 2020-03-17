ec927b66216d216baa1eed1175db03266d4393f2
grabinow2 <grabinow2@student.touro.edu>
2020-03-17 16:38:22 -0400

    linked the second menu item in menu.php to the login page

--- php/menu.php
+++ php/menu.php
@@ -1,7 +1,7 @@
 <P>THE MENU</P>
 <ul>
     <li><a href="form.php">Form</a>
-    <li><a href="...">Second Page</a>
+    <li><a href="login.php">Login</a>
     <li><a href="...">Third Page</a>
     <li><a href="...">Fourth Page</a>
 </ul>
