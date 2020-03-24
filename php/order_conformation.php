65b863347b77c06a438a4c03599c88e615fa4548
grabinow2 <grabinow2@student.touro.edu>
2020-03-24 15:19:20 -0400

    Oh also this

--- php/order_conformation.php
+++ php/order_conformation.php
@@ -1,5 +1,7 @@
-<LINK rel="stylesheet" href="../css/order_conformation_styles.css" type="text/css" />
-Thank you for placing your order!
+<?php
+include "header.php";
+?>
+you for placing your order!
 <br>
 <br>
 You will receive an email shortly with more information.
@@ -7,3 +9,6 @@ You will receive an email shortly with more information.
 <br>
 <br>
 Keep On Shinning!!!
+<?php
+include "footer.php";
+?>
