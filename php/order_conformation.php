e0ebf362cbbc782d8b82b8ecf6d429f40ba25ba4
Steven G <reuven9899@gmail.com>
2020-03-24 11:56:42 -0400

    Added Order Conformation Form

---
+++ php/order_conformation.php
@@ -0,0 +1,9 @@
+<LINK rel="stylesheet" href="../css/order_conformation_styles.css" type="text/css" />
+Thank you for placing your order!
+<br>
+<br>
+You will receive an email shortly with more information.
+<br>
+<br>
+<br>
+Keep On Shinning!!!
