b60f4c5d8b5c4f18262a6f40610c3da9710ff53c
Steven G <reuven9899@gmail.com>
2020-03-24 11:52:12 -0400

    Added Order Form

---
+++ php/order_page.php
@@ -0,0 +1,33 @@
+<LINK rel="stylesheet" href="../css/order_form_styles.css" type="text/css" />
+Enter Your Information Below:
+<br>
+<br>
+<br>
+<label for="Full_Name"><b>Full Name</b></label>
+<input type="text" placeholder="Enter Full Name" name="Full_Name" required>
+<br>
+<label for="Regions">Choose a Country:</label>
+<select id="Regions">
+    <option value="Test for G.0 #Chaverim4Life">G.O</option>
+</select>
+<br>
+<label for="State"><b>State</b></label>
+<input type="text" placeholder="Enter State" name="State" required>
+<br>
+<label for="Address"><b>Address</b></label>
+<input type="text" placeholder="Enter Address" name="Address" required>
+<br>
+<label for="Zip_Code"><b>Zip Code</b></label>
+<input type="text" placeholder="Enter Zip Code" name="Zip_Code" required>
+<br>
+<label for="Payment">Choose a Payment Option:</label>
+<select id="Payment">
+    <option value="Test for G.0 #Chaverim4Life">G.O</option>
+</select>
+<br>
+<label for="Email"><b>Email</b></label>
+<input type="text" placeholder="Enter Email" name="Email" required>
+<br>
+<input type="checkbox" id="Receive_Emails_CB" name="Receive_Emails" value="Receive">
+<label for="Receive_Emails_CB"> Receive Emails</label><br>
+
