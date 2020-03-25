45b552b84f6c10b30179abb5c9b332a80d990131
Jacob Burstyn <jacobburstyn@gmail.com>
2020-03-24 21:18:26 -0400

    added a emailList table which I used to send some of the form info to and took from to display in displayDBtable

--- MyStar.sql
+++ MyStar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.4
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2020 at 08:13 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.3.0
+-- Generation Time: Mar 25, 2020 at 02:15 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `MyStar`
+-- Database: `mystar`
 --
 
 -- --------------------------------------------------------
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `authorizedusers` (
-  `userName` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `CUST_ID` int(11) NOT NULL
+                                   `userName` varchar(255) NOT NULL,
+                                   `password` varchar(255) NOT NULL,
+                                   `CUST_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -53,13 +53,34 @@ INSERT INTO `authorizedusers` (`userName`, `password`, `CUST_ID`) VALUES
 --
 
 CREATE TABLE `customer` (
-  `CUST_ID` int(11) NOT NULL,
-  `F_NAME` varchar(20) NOT NULL,
-  `L_NAME` varchar(20) NOT NULL,
-  `DT_JOINED` date NOT NULL,
-  `EMAIL` varchar(64) NOT NULL,
-  `PAYMENT_ID` int(11) NOT NULL
+                            `CUST_ID` int(11) NOT NULL,
+                            `F_NAME` varchar(20) NOT NULL,
+                            `L_NAME` varchar(20) NOT NULL,
+                            `DT_JOINED` date NOT NULL,
+                            `EMAIL` varchar(64) NOT NULL,
+                            `PAYMENT_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emaillist`
+--
+
+CREATE TABLE `emaillist` (
+                             `email` varchar(255) NOT NULL,
+                             `first name` varchar(255) NOT NULL,
+                             `last name` varchar(255) NOT NULL,
+                             `receive` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `emaillist`
+--
+
+INSERT INTO `emaillist` (`email`, `first name`, `last name`, `receive`) VALUES
+('Jacobburstyn@gmail.com', 'Jacob', 'Burstyn', 1),
+('', '', '', 0);
 
 -- --------------------------------------------------------
 
@@ -68,12 +89,12 @@ CREATE TABLE `customer` (
 --
 
 CREATE TABLE `payment_info` (
-  `PAYMENT_ID` int(11) NOT NULL,
-  `payment_type_ID` int(11) NOT NULL,
-  `NAME_ON_CARD` varchar(40) NOT NULL,
-  `CC_NUM` varchar(19) NOT NULL,
-  `EXP_DT` varchar(5) NOT NULL,
-  `CCV` varchar(5) NOT NULL
+                                `PAYMENT_ID` int(11) NOT NULL,
+                                `payment_type_ID` int(11) NOT NULL,
+                                `NAME_ON_CARD` varchar(40) NOT NULL,
+                                `CC_NUM` varchar(19) NOT NULL,
+                                `EXP_DT` varchar(5) NOT NULL,
+                                `CCV` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -83,8 +104,8 @@ CREATE TABLE `payment_info` (
 --
 
 CREATE TABLE `payment_types` (
-  `payment_type_ID` int(11) NOT NULL,
-  `payment_type` varchar(20) NOT NULL
+                                 `payment_type_ID` int(11) NOT NULL,
+                                 `payment_type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -104,10 +125,10 @@ INSERT INTO `payment_types` (`payment_type_ID`, `payment_type`) VALUES
 --
 
 CREATE TABLE `purchase` (
-  `PURCHASE_ID` int(11) NOT NULL,
-  `CUST_ID` int(11) NOT NULL,
-  `STAR_ID` int(11) NOT NULL,
-  `PURCHASE_DT` date NOT NULL
+                            `PURCHASE_ID` int(11) NOT NULL,
+                            `CUST_ID` int(11) NOT NULL,
+                            `STAR_ID` int(11) NOT NULL,
+                            `PURCHASE_DT` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -117,8 +138,8 @@ CREATE TABLE `purchase` (
 --
 
 CREATE TABLE `selling_regions` (
-  `Selling_region_ID` int(11) NOT NULL,
-  `REGION` varchar(20) NOT NULL
+                                   `Selling_region_ID` int(11) NOT NULL,
+                                   `REGION` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -142,11 +163,11 @@ INSERT INTO `selling_regions` (`Selling_region_ID`, `REGION`) VALUES
 --
 
 CREATE TABLE `star` (
-  `STAR_ID` int(11) NOT NULL,
-  `NAME` varchar(30) NOT NULL,
-  `DT_UPLOADED` date NOT NULL,
-  `PRICE` decimal(10,0) NOT NULL,
-  `DESCRIPTION` text NOT NULL
+                        `STAR_ID` int(11) NOT NULL,
+                        `NAME` varchar(30) NOT NULL,
+                        `DT_UPLOADED` date NOT NULL,
+                        `PRICE` decimal(10,0) NOT NULL,
+                        `DESCRIPTION` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -157,37 +178,37 @@ CREATE TABLE `star` (
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
-  ADD PRIMARY KEY (`CUST_ID`);
+    ADD PRIMARY KEY (`CUST_ID`);
 
 --
 -- Indexes for table `payment_info`
 --
 ALTER TABLE `payment_info`
-  ADD PRIMARY KEY (`PAYMENT_ID`);
+    ADD PRIMARY KEY (`PAYMENT_ID`);
 
 --
 -- Indexes for table `payment_types`
 --
 ALTER TABLE `payment_types`
-  ADD PRIMARY KEY (`payment_type_ID`);
+    ADD PRIMARY KEY (`payment_type_ID`);
 
 --
 -- Indexes for table `purchase`
 --
 ALTER TABLE `purchase`
-  ADD PRIMARY KEY (`PURCHASE_ID`);
+    ADD PRIMARY KEY (`PURCHASE_ID`);
 
 --
 -- Indexes for table `selling_regions`
 --
 ALTER TABLE `selling_regions`
-  ADD PRIMARY KEY (`Selling_region_ID`);
+    ADD PRIMARY KEY (`Selling_region_ID`);
 
 --
 -- Indexes for table `star`
 --
 ALTER TABLE `star`
-  ADD PRIMARY KEY (`STAR_ID`);
+    ADD PRIMARY KEY (`STAR_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -197,37 +218,37 @@ ALTER TABLE `star`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `CUST_ID` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `CUST_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `payment_info`
 --
 ALTER TABLE `payment_info`
-  MODIFY `PAYMENT_ID` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `PAYMENT_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `payment_types`
 --
 ALTER TABLE `payment_types`
-  MODIFY `payment_type_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `payment_type_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `PURCHASE_ID` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `PURCHASE_ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `selling_regions`
 --
 ALTER TABLE `selling_regions`
-  MODIFY `Selling_region_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+    MODIFY `Selling_region_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `star`
 --
 ALTER TABLE `star`
-  MODIFY `STAR_ID` int(11) NOT NULL AUTO_INCREMENT;
+    MODIFY `STAR_ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
