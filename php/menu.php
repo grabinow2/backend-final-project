ad14c23ea73561c897da46eb06810aaeca6da6c4
grabinow2 <grabinow2@student.touro.edu>
2020-04-07 11:18:09 -0400

    Just reorganized some project files into directories.

--- php/menu.php
+++ php/menu.php
@@ -1,5 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<LINK rel="stylesheet" href="menu.css" type="text/css">
+<LINK rel="stylesheet" href="../css/menu.css" type="text/css">
 <ul id="navMenu">
     <li><a href="form.php">Form</a>
     <li><a href="login.php">Login</a>
@@ -7,7 +7,7 @@
     <li><a href="...">Fourth Page</a>
 </ul>
 
-<script src="menu.js"></script>
+<script src="../js/menu.js"></script>
 <script>
     $('li a').css("color","red")
 </script>
